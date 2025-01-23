0c014b9ea934756210763cea61372a5e6f8f52ea
Vovede <zarubin.fr@gmail.com>
2025-01-23 20:53:51 +0300

    main

--- main.py
+++ main.py
@@ -1,37 +1,6 @@
+from pick_block_parameter import pick_shape
+from pick_block_parameter import pick_color
 import pygame
-import random
-
-shapes = [
-    [[1, 1],
-     [1, 0]],
-
-    [[1, 1],
-     [0, 1]],
-
-    [[1, 1, 1],
-     [0, 1, 0]],
-
-    [[1, 1],
-     [1, 1]],
-
-    [[1, 1, 1, 1]],
-
-    [[1],
-     [1],
-     [1],
-     [1]],
-
-    [[1, 1],
-     [1, 0],
-     [1, 0]]
-]
-
-colors = [
-    pygame.Color("red"),
-    pygame.Color("green"),
-    pygame.Color("blue"),
-    pygame.Color("yellow")
-]
 
 
 class Block:
@@ -41,18 +10,19 @@ class Block:
         self.width = len(shape[0])
         self.height = len(shape)
         self.position = (0, 0)
+        self.block_origin = (0, 0)
+        self.is_placed = False
 
     def set_position(self, x, y):
         self.position = (x, y)
 
-    def draw(self, screen, cell_size, offset):
-        ox, oy = offset
+    def draw(self, screen, cell_size):
         for y, row in enumerate(self.shape):
             for x, cell in enumerate(row):
                 if cell:
                     pygame.draw.rect(screen, self.color,
-                                     (self.position[0] + x * cell_size + ox,
-                                      self.position[1] + y * cell_size + oy,
+                                     (self.position[0] + x * cell_size,
+                                      self.position[1] + y * cell_size,
                                       cell_size, cell_size))
 
     def is_mouse_over_block(self, mouse_pos, cell_size):
@@ -61,12 +31,35 @@ class Block:
         for y, row in enumerate(self.shape):
             for x, cell in enumerate(row):
                 if cell:
-                    hitbox = pygame.Rect(block_x + (x + 1) * cell_size,
-                                         block_y + (y + 1) * cell_size,
+                    hitbox = pygame.Rect(block_x + x * cell_size,
+                                         block_y + y * cell_size,
                                          cell_size, cell_size)
                     if hitbox.collidepoint(mouse_x, mouse_y):
                         return True
         return False
+
+    def snap_to_grid(self, cell_size, grid_offset, grid_width, grid_height, board):
+        gx, gy = grid_offset
+        px, py = self.position
+
+        block_snapped_to_x = round((px - gx) / cell_size) * cell_size + gx
+        block_snapped_to_y = round((py - gy) / cell_size) * cell_size + gy
+
+        for y, row in enumerate(self.shape):
+            for x, cell in enumerate(row):
+                if cell:
+                    cell_pos_in_grid_x = (block_snapped_to_x - gx) // cell_size + x
+                    cell_pos_in_grid_y = (block_snapped_to_y - gy) // cell_size + y
+
+                    if (cell_pos_in_grid_x < 0 or cell_pos_in_grid_x >= grid_width or
+                            cell_pos_in_grid_y < 0 or cell_pos_in_grid_y >= grid_height):
+                        self.set_position(*self.block_origin)
+                        return
+
+        self.set_position(block_snapped_to_x, block_snapped_to_y)
+        self.is_placed = True
+        if not board.check_block(self):
+            self.set_position(*self.block_origin)
 
 
 class Board:
@@ -95,17 +88,40 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
-    def add_block(self, block, position):
-        px, py = position
-        for y in range(block.height):
-            for x in range(block.width):
-                if block.shape[y][x] == 1:
-                    self.board[py + y][px + x] = 1
+    def check_block(self, block):
+        px, py = block.position
+        grid_x = (px - self.left) // self.cell_size
+        grid_y = (py - self.top) // self.cell_size
+
+        for y, row in enumerate(block.shape):
+            for x, cell in enumerate(row):
+                if cell:
+                    board_x = grid_x + x
+                    board_y = grid_y + y
+
+                    if not (0 <= board_x < self.width and 0 <= board_y < self.height):
+                        return False
+                    if self.board[board_y][board_x] == 1:
+                        return False
+        return True
+
+    def add_block(self, block):
+        if not self.check_block(block):
+            return False
+        px, py = block.position
+        grid_x = (px - self.left) // self.cell_size
+        grid_y = (py - self.top) // self.cell_size
+
+        for y, row in enumerate(block.shape):
+            for x, cell in enumerate(row):
+                if cell and 0 <= grid_x + x < self.width and 0 <= grid_y + y < self.height:
+                    self.board[grid_y + y][grid_x + x] = 1
+        return True
 
 
 def generate_random_block():
-    shape = random.choice(shapes)
-    color = random.choice(colors)
+    shape = pick_shape()
+    color = pick_color()
     return Block(shape, color)
 
 
@@ -119,9 +135,17 @@ def main():
     size = ((board.width + 2) * board.left, (board.height + 6) * board.top)
     screen = pygame.display.set_mode(size)
 
-    current_block = generate_random_block()
+    blocks = [generate_random_block() for _ in range(3)]
+    block_area_y = size[1] - 200
+    block_positions = [(i * 150 + 50, block_area_y) for i in range(3)]
+
+    for block, pos in zip(blocks, block_positions):
+        block.block_origin = pos
+        block.set_position(*pos)
+
     running = True
-    dragging = False
+    dragging = None
+    drag_offset = (0, 0)
 
     while running:
         for event in pygame.event.get():
@@ -129,20 +153,32 @@ def main():
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    if current_block.is_mouse_over_block(event.pos, board.cell_size):
-                        dragging = True
+                    for block in blocks:
+                        if block.is_mouse_over_block(event.pos, board.cell_size):
+                            if not block.is_placed:
+                                dragging = block
+                                mx, my = event.pos
+                                bx, by = block.position
+                                drag_offset = (mx - bx, my - by)
+                                break
             elif event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    dragging = False
+                if event.button == 1 and dragging:
+                    dragging.snap_to_grid(board.cell_size, (board.left, board.top), board.width,
+                                          board.height, board)
+                    if board.check_block(dragging):
+                        board.add_block(dragging)
+                    dragging = None
             elif event.type == pygame.MOUSEMOTION:
                 if dragging:
-                    current_block.set_position(event.pos[0] - board.cell_size,
-                                               event.pos[1] - board.cell_size)
+                    mx, my = event.pos
+                    offset_x, offset_y = drag_offset
+                    dragging.set_position(mx - offset_x, my - offset_y)
 
         screen.fill((50, 130, 255))
         board.render(screen)
 
-        current_block.draw(screen, board.cell_size, (board.left, board.top))
+        for block in blocks:
+            block.draw(screen, board.cell_size)
 
         pygame.display.flip()
 
